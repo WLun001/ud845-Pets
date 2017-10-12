eaa27861f5bc0fd35427ef7f2522d1d8c6e90e15
Wei Lun <weilun_95@hotmail.com>
2017-10-12 21:07:19 +0800

    added Toast message when deletion

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -122,8 +122,8 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
                 return true;
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
-                getContentResolver().delete(PetEntry.CONTENT_URI, null, null);
-                Toast.makeText(this, "All records have been deleted", Toast.LENGTH_SHORT).show();
+                int rowDeleted = getContentResolver().delete(PetEntry.CONTENT_URI, null, null);
+                Toast.makeText(this, "All records have been deleted, rows affected: " + rowDeleted, Toast.LENGTH_SHORT).show();
                 return true;
         }
         return super.onOptionsItemSelected(item);
